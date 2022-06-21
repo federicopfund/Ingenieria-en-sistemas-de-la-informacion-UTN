893793880aa3d45519d98076360b3635d91dcdc0
federico pfund <federicopfund@gmail.com>
2022-06-21 12:32:03 -0300

    procedimientos practco 5.1

--- Algoritmos_Estructuras_Datos/UTN_Practicos_2022/Resoluciones/ejercicio5.1/modulariza.py
+++ Algoritmos_Estructuras_Datos/UTN_Practicos_2022/Resoluciones/ejercicio5.1/modulariza.py
@@ -8,25 +8,27 @@
 
 
 
-def cuadradolocals( numero):
-    
+
+
+
+from ast import Global
+from threading import local
+
+
+def cuadradolocals(numero):
+    ##numero = globals
+    numero = locals()["numero"]
     ## ingresar datos, captar exepciones con try
-    def cargar_numero():
-        nonlocal numero
-        try:
-            numero = int(input("Ingrese un numero: "))
-            return numero
-        except ValueError:
-            print("Error, no es un numero")
-            cargar_numero()
-       
+          
     def elevar(numero):
         return numero ** 2
  
-    def imprime(numero):
-        print(f"El {numero} al cuadrado es: {elevar(cargar_numero())}") 
+    def imprime(numero): 
+        print(f"El {numero} al cuadrado es: {elevar(numero)}") 
+        
+    imprime(numero)
     ## verificar como ingresar al valor de numero que esta guardado en memoria.
-    imprime(cargar_numero())
+    
 
 
 
@@ -36,11 +38,67 @@ def cuadradolocals( numero):
 ## Mostrar el valor de n, elevado al cuadrado desde dentro del procedimiento.
 ## Modificar el valor de n, dentro del procedimiento. ¿Que ocurre?
 ## Si necesitase modificar el valor de n, que es una variable global, dentro del procedimiento cuadrado, ¿Que deberıa hacer?
-globals()['valor'] = 30 #modificamos el valor de la variable a través del diccionario
+
+
 def cuadradoglobal():
+    # creamos una variable global
     global n
+    
+    ## acceder a la variable global y cambiar el valor de la misma
+    globals()["n"] =6
+   
     cuadradolocals(n)
     ## para modificar el valor de n, dentro del procedimiento, se debe utilizar el nombre de la variable
-    globals()['n'] = 30 
+    globals()['n'] = 30  #modificamos el valor de la variable a través del diccionario
     cuadradolocals(n)
-cuadradoglobal()
+   
+
+## ¿Que imprimira en pantalla el siguiente codigo?
+# Output :"Es un lindo dia"
+#  ¿Cual es el alcance de la variable frase?
+## el alcance de la variable frase es local, porque esta dentro del procedimiento f().
+## tan local  que si imprimo el nombre de la variable frase antes de la asignacion dentro del proceso 
+#   el papu no save la asignacion que tiene frase y su Output dira : "UnboundLocalError" claro que si el no 
+## entiende nada sobre lo que le estampos pidiendo pero la raza humana se encargara de ayudarlo
+#  podriamos darle una mano y explicarle que es una variable local.
+frase = "Hola"
+
+def f():
+    try:
+        print(frase)
+        frase = "Es un lindo dia"
+        print(frase)
+    except UnboundLocalError as error:
+        print("Su variable no esta definida dentro del procedimiento")
+        frase = "Es un lindo dia"
+        f() 
+
+
+## ¿Que imprimira en pantalla el siguiente codigo? 
+## Output: "3" "4" "1"
+# Determine el alcance de cada variable.
+# alcance de x: global
+# alcance de y: local pero bueno porque esta dentro de la funcion f() definida.
+# osea que x es mas global que y.
+# en todo caso sera y global en todo el procedimiento.
+
+x = 3
+
+def f():
+    
+    y = x + 1
+    ## nuuuu encontro el valor de x el manija, la verdad que no se porque pense que no se puede acceder a x desde esta anidacion.
+    print(x)
+    def g():
+        x = 1
+        print(y)
+        print(x)
+        def h():
+            z = y + 1
+            print(z)
+            print(y)
+        h()
+        # print(z) aca si no entiende y no sabe que es z, ya que z es local re contra local.
+    g()
+f()
+

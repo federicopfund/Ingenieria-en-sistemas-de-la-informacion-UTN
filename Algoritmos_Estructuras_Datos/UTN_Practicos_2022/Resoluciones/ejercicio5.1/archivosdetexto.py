dbf9b455cd974b9a814bd92b7fbe00b8601dadb5
federico pfund <federicopfund@gmail.com>
2022-06-22 23:57:41 -0300

    mudulariza

---
+++ Algoritmos_Estructuras_Datos/UTN_Practicos_2022/Resoluciones/ejercicio5.1/archivosdetexto.py
@@ -0,0 +1,26 @@
+## Crear un archivo .txt , y guardar una palabra por
+## lınea. Crear luego un programa que muestre en pantalla, una por una, las palabras que
+## hay guardadas en ese archivo. Mostrar ademas, el total de lıneas que habrıa en el archivo.
+
+
+from pathlib import Path
+
+
+def crearachivo():
+    p1 = Path('.')
+    print(p1.absolute())
+    
+    #myfile = Path(__file__).parent / 'data' / 'mail.txt'
+  #  myfile.touch(exist_ok=True)
+    
+
+crearachivo()
+
+# crea un archivo .txt
+def crea():
+    
+
+
+
+
+    

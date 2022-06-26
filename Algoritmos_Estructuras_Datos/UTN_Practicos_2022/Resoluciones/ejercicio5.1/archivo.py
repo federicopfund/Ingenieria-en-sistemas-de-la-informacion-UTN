73361c5a47a06b2457c204150bdbece61632bb79
federico pfund <federicopfund@gmail.com>
2022-06-26 14:47:31 -0300

    manejo de archivos .txt

--- Algoritmos_Estructuras_Datos/UTN_Practicos_2022/Resoluciones/ejercicio5.1/archivo.py
+++ Algoritmos_Estructuras_Datos/UTN_Practicos_2022/Resoluciones/ejercicio5.1/archivo.py
@@ -1,5 +1,4 @@
-## Crear un archivo de texto .txt de notas de Windows, y guardar una palabra por lınea.
-## Crear un programa que muestre en pantalla, una por una, las palabras que hay guardadas en ese archivo.
+
 from distutils import extension
 
 
@@ -15,11 +14,12 @@ def archivo():# podriamos mandar un argumento pdre para que gobierne todas las f
             return None
 
         # escribir en el archivo
-    def escribir_archivo(nombre, texto):
+    def escribir_archivo(nombre):
         try:
            cantidad = int(input('Cuantas lineas deceas escribir: '))
            with open(nombre, "w+") as archivo:
-               for i in range(cantidad):
+               for i in range(cantidad-1):
+                   texto = input(f'Ingrese un {nombre.strip("s.txt")}: ')
                    archivo.write(f'LINEA:{i}: {texto}\n')
         except OSError :
             print("No se pudo escribir en el archivo")
@@ -37,14 +37,9 @@ def archivo():# podriamos mandar un argumento pdre para que gobierne todas las f
         extencion = input(f"Ingrese la extencion del archivo: ")
         nombre = ingrese_nombre + "." + extencion
         crear_archivo(nombre) 
-        texto = input(f"Ingrese el texto que desea guardar en el archivo: ")
-        escribir_archivo(nombre, texto)       
+        escribir_archivo(nombre)       
         mostrar_leer_archivo(nombre)  
     main_hijo()        
-
-    
-# Crear un programa que genere un archivo de texto llamado numeros.txt con 10 numeros
-# enteros guardados en el mismo, uno por lınea.
 
 def archivo_numeros():
     try:
@@ -69,10 +64,73 @@ def archivo_numeros():
         crea_archivo_algoritmen(nombre,cantidad)
         leer_algoritmen(nombre)
     main_hijo("numeros",10,"txt")
+
+def archivo_colores():
+    ## reutilizamos codigo.
+    archivo()
+
+def archivo_numeros():
+    import random
+    def crear_archivo_como(nombre,extension):
+        nombre= nombre + "." + extension
+        with open(nombre, "a") as numero:
+            return numero
+    ## llenamos  archivo con numeros random   
+    def llenar_archivo(nombre,cantidad):
+         with open(nombre, "w+") as f:
+            for i in range(cantidad):
+                numero = random.randint(1,10)
+                f.write(f'{numero}\n')
+    ## leemos archivo
+    def leer_archivo_compara(nombre):
+        contador=0
+        with open(nombre, 'r') as f:
+            lineas = f.readlines()
+            for linea in lineas:
+                if linea.strip() == '5':
+                    contador += 1
+        print(f'Cantidad de numeros iguales a 5: {contador}')
+        
+    def main_hijo():
+        nombre = input("Ingrese el nombre del archivo: ")
+        extension = input("Ingrese la extencion del archivo: ")    
+        nombre = nombre + "." + extension
+        crear_archivo_como(nombre,extension)
+        cantidad = int(input("Ingrese la cantidad de numeros que desea guardar: "))
+        llenar_archivo(nombre,cantidad)
+        leer_archivo_compara(nombre)
+    main_hijo()
+
+
+def archivo_promedio(nombre):
+    cuenta=0
     
-archivo_numeros()
+    with open (nombre,'r+') as promedio:
+        linea=promedio.readlines()
 
-
-
-
+        for i in range(len(linea)):
+            linea[i]=int(linea[i].rstrip("\n"))
+            cuenta = linea[i] + cuenta
+        promedio=cuenta/len(linea)
+    print(f'promedio de archivos: {round(promedio,2)}.')
     
+
+
+def main():
+    print("""Crear un archivo de texto .txt de notas de Windows, y guardar una palabra por lınea.
+            Crear un programa que muestre en pantalla,una por una, 
+            las palabras que hay guardadas en ese archivo.""")
+    archivo()
+    print("""Crear un programa que genere un archivo de texto llamado numeros.txt con 10 numeros
+            enteros guardados en el mismo, uno por lınea.""")
+    archivo_numeros()
+    print("""Crear un programa que le pida al usuario 5 colores, y los guarde en un archivo de texto
+            llamado colores.txt""")
+    archivo_colores()
+    print(""" Crear un programa que dado un archivo de numeros con valores entre 1 y 10 (lo puede generar como ud. desee)
+                determine cuantos numeros iguales a 5 hay en el archivo.""")
+    archivo_promedio('numeros.txt')
+
+
+if __name__ == "__main__":
+    main()

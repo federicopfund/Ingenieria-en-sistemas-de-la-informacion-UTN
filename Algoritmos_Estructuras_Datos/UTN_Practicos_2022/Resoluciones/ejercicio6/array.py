8891f2e4ba5ee458136ae72982d3823881529044
federico pfund <federicopfund@gmail.com>
2022-06-29 20:36:00 -0300

    Numpy documentacion

--- Algoritmos_Estructuras_Datos/UTN_Practicos_2022/Resoluciones/ejercicio6/array.py
+++ Algoritmos_Estructuras_Datos/UTN_Practicos_2022/Resoluciones/ejercicio6/array.py
@@ -2,9 +2,10 @@
 # por ejemplo [181.5, 72., 34.7, 171.3, 160.1]. Crear un array y mostrar sus atributos, el tipo
 # de datos, tanto del array como de sus elementos. Mostrar tambien el total de familiares
 # cargados en el array.
+import numpy as np
 
 def familia():
-    alturas = [181.5, 72., 34.7, 171.3, 160.1]
+    alturas = np.array([181.5, 72., 34.7, 171.3, 160.1])
     print(alturas)
     print(f'tipo de datos del array:{type(alturas)}')
     for altura in alturas:
@@ -12,9 +13,16 @@ def familia():
         print(f'Tipo de datos del array: {type(altura)}')
     print(f'Total de familiares: {len(alturas)}')
 
-# Crear un array de 3 dimensiones, que tenga 3 matrices de 2 filas por 4 columnas. Llenelo
-# con ceros.
 
+def array3d(dimencion,filas,columnas):
+    # crear un array de 3 dimensiones, que tenga 3 matrices de 2 filas por 4 columnas. Llenelo
+    # con ceros.
+    array=np.zeros((dimencion,filas,columnas))
+    print(f'matriz de Zeros{array}')
+    print(f'Tupla de {array.shape}')
+    print(array.ndim)
+array3d(3,2,3)   
+    
 def array(dimencion):
     array = []
     for i in range(dimencion):
@@ -27,3 +35,4 @@ def array(dimencion):
             print(f'Ingresamos al elementos del array: {array[i][j]}')
             print(f'Tipo de datos del array: {type(array[i][j])}')
     return array
+
